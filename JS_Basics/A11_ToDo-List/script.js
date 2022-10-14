d1178872590262eaa716128221f16daa9dcd328e
Mert <martinschneider22@gmx.net>
2022-10-14 15:59:59 +0200

    todo extended with classes

--- JS_Basics/A11_ToDo-List/script.js
+++ JS_Basics/A11_ToDo-List/script.js
@@ -4,19 +4,41 @@ function FillTodoListFromArray() {
     console.log(listData);
     document.getElementById("todoList").innerHTML = "";
     listData.forEach(element => {
-        document.getElementById("todoList").innerHTML += "<li>" + element + "</li>"; 
+      let isChecked = (element.isDone ? " checked" : " ");
+      document.getElementById("todoList").innerHTML += "<li><input type='checkbox' class='taskCheckbox'"+isChecked+"/>" + 
+        element.name + " - " + element.responsible + 
+        " <button class='deleteItem'>X</button></li>"; 
     })
+
+    var checkboxes = document.getElementsByClassName("taskCheckbox");
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].addEventListener("change", function(event) {
+        listData[i].isDone = !listData[i].isDone;
+        console.log(listData[i].name + " - " + listData[i].isDone);
+      });
+    };
+    
+    var deleteItems = document.getElementsByClassName("deleteItem");
+    for (let i = 0; i < deleteItems.length; i++) {
+      deleteItems[i].addEventListener("click", function(event) {
+        listData.splice(i, 1); 
+        FillTodoListFromArray();
+      });
+    };
 }
 
-document.getElementById("saveInput").addEventListener("keypress", function(event) {
+/*document.getElementById("saveInput").addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       event.preventDefault();
       document.getElementById("storeTodo").click();
     }
-  });
+  });*/
 
 document.getElementById("storeTodo").addEventListener("click", function() {
-    listData.push(document.getElementById("saveInput").value);
+    let taskName = document.getElementById("saveInput").value;
+    let personResponsible = document.getElementById("saveResponsible").value;
+    let task = {name:taskName, responsible: personResponsible, isDone: false};
+    listData.push(task);
     FillTodoListFromArray();
 });
 
